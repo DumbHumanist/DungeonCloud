4741fcfaf6ad27cab00bdaf5829fb34de3b27480
MaksimMogylnik <maks.mogylnik03@gmail.com>
2021-04-21 16:16:49 +0300

    user space back

---
+++ DungeonCloud/DungeonCloud/Models/FileSystemInfoExt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCloud.Models
+{
+    class FileSystemInfoExt
+    {
+        public string ImageSource { get; set; }
+
+        public FileSystemInfo FSI { get; set; }
+
+        public override string ToString() => FSI.Name;
+    }
+}
